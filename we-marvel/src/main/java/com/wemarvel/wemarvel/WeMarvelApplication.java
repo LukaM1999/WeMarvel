d1605b9f0414a00e73569d48974b510f530ce378
LukaM1999 <miletic.ra194.2018@uns.ac.rs>
2022-07-16 14:42:34 +0200

    Initialized Spring Boot application

---
+++ we-marvel/src/main/java/com/wemarvel/wemarvel/WeMarvelApplication.java
@@ -0,0 +1,13 @@
+package com.wemarvel.wemarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeMarvelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeMarvelApplication.class, args);
+    }
+
+}
